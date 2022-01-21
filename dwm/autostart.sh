a3fcd5b2d89d23c313974ea4377f7d3c02583f68
Taofeek Wahab <samtrek@yahoo.com>
2022-01-21 07:59:28 +0300

    updating dwm to make it launch faster

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -5,9 +5,6 @@ dwmblocks &
 nm-applet &
 setxkbmap tr -option caps:escape &
 
-sleep 20 && caffeine &
-sleep 60 && pcmanfm -d &
-
 if pgrep -x python3 > /dev/null; then
 	exit 1
 else
@@ -26,6 +23,19 @@ else
 	sleep 20 && goldendict &
 fi
 
+if pgrep -x caffeine > /dev/null; then
+	exit 1
+else
+	sleep 20 && caffeine &
+fi
+
+if pgrep -x pcmanfm > /dev/null; then
+	exit 1
+else
+	sleep 60 && pcmanfm -d &
+fi
+
+
 if pgrep -x whatscli > /dev/null; then
 	exit 1
 else
@@ -37,4 +47,3 @@ if pgrep -x whatscli > /dev/null; then
 else
 	sleep 10 && kitty --name "joplin" -o initial_window_width=65c -o initial_window_height=14c -e joplin &
 fi
-
