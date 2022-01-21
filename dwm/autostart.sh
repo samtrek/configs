be4200c43470b08640293e2db880735766ac74dd
Taofeek Wahab <samtrek@yahoo.com>
2022-01-21 20:32:25 +0300

    finally solved the starting of apps with dwm

--- dwm/autostart.sh
+++ dwm/autostart.sh
@@ -1,55 +1,54 @@
 #!/bin/sh
+
 $HOME/.layout.sh &
 $HOME/.fehbg; picom -b &
 nm-applet &
 setxkbmap tr -option caps:escape &
 
-if pgrep -x python3 > /dev/null; then
-	exit 1
-else
-	sleep 30 && calibre --start-in-tray &
-fi
-
-if pgrep -x dwmblocks > /dev/null; then
-	exit 1
+if pgrep -f dwmblocks > /dev/null; then
+	exit 1 &
 else
 	dwmblocks &
 fi
 
+if pgrep -f calibre > /dev/null; then
+	exit 1 &
+else
+	sleep 30 && calibre --start-in-tray &
+fi
 
-if pgrep -x python3 > /dev/null; then
-	exit 1
+if pgrep -x xautolock > /dev/null; then
+	exit 1 & 
 else
 	 xautolock -time 10 -locker slock &
 fi
 
 if pgrep -x goldendict > /dev/null; then
-	exit 1
+	exit 1 &
 else
 	sleep 20 && goldendict &
 fi
 
-if pgrep -x caffeine > /dev/null; then
-	exit 1
+if pgrep -f caffeine > /dev/null; then
+	exit 1 &
 else
 	sleep 20 && caffeine &
 fi
 
-if pgrep -x pcmanfm > /dev/null; then
-	exit 1
+if pgrep -x pcmanfm-qt > /dev/null; then
+	exit 1 &
 else
-	sleep 60 && pcmanfm -d &
+	sleep 60 && pcmanfm-qt -d &
 fi
 
-
 if pgrep -x whatscli > /dev/null; then
-	exit 1
+	exit 1 &
 else
 	sleep 10 && kitty --name "whatscli" -o initial_window_width=65c -o initial_window_height=14c -e whatscli &
 fi
 
-if pgrep -x whatscli > /dev/null; then
-	exit 1
+if pgrep -f joplin  > /dev/null; then
+	exit 1 &
 else
 	sleep 10 && kitty --name "joplin" -o initial_window_width=65c -o initial_window_height=14c -e joplin &
 fi
