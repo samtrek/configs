cfa9abbe755a66d7b53f54c7ec79db3d30da1ab7
samtrek <samtrek@yahoo.com>
2023-06-06 22:06:40 +0300

    made changes to rofi config to switch between night and day

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -3,3 +3,4 @@ pkill -USR1 zsh
 theme=Graphite-teal-Dark-compact-nord
 gsettings set org.gnome.desktop.interface gtk-theme $theme
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
+sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord-dark\"/" ~/.config/rofi/config.rasi
--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -3,3 +3,4 @@ pkill -USR1 zsh
 theme=Graphite-teal-compact-nord
 gsettings set org.gnome.desktop.interface gtk-theme $theme
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
+sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord.rasi\"/" ~/.config/rofi/config.rasi
