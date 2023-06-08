e94b658c1a434c049133e843ed8eb25f7a4a2e19
Taofeek Wahab <samtrek@yahoo.com>
2023-06-08 17:20:56 +0300

    finally made uniform dark and light switching, with xsettingsd

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -5,3 +5,7 @@ gsettings set org.gnome.desktop.interface gtk-theme $theme
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord-dark\"/" ~/.config/rofi/config.rasi
 brightnessctl set 25%
+sed -i "s/^gtk-theme-name=.*/gtk-theme-name=\"$theme\"/" ~/.gtkrc-2.0
+sed -i "s/^Net\/ThemeName.*/Net\/ThemeName \"$theme\"/" ~/.config/xsettingsd/xsettingsd.conf
+pkill xsettingsd
+xsettingsd &
