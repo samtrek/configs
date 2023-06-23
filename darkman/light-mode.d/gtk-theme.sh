eb25188ec9b54331033aa6f4c559c44c0e5be582
samtrek <samtrek@yahoo.com>
2023-06-23 07:48:44 +0300

    switched to another form of theme

--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -1,15 +1,15 @@
 #! /bin/sh
-theme=Graphite-teal-Light-compact-nord
+theme=Nordic-Polar
 icontheme=Papirus-Light
 pkill -USR1 zsh
-gsettings set org.gnome.desktop.interface gtk-theme $theme
-gsettings set org.gnome.desktop.interface icon-theme $icontheme
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^gtk-icon-theme-name=.*/gtk-icon-theme-name=$icontheme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord.rasi\"/" ~/.config/rofi/config.rasi
 brightnessctl set 75%
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=\"$theme\"/" ~/.gtkrc-2.0
-sed -i "s/^gtk-icon-theme-name=.*/gtk-theme-name=\"$icontheme\"/" ~/.gtkrc-2.0
+sed -i "s/^gtk-icon-theme-name=.*/gtk-icon-theme-name=\"$icontheme\"/" ~/.gtkrc-2.0
 sed -i "s/^Net\/ThemeName.*/Net\/ThemeName \"$theme\"/" ~/.config/xsettingsd/xsettingsd.conf
 sed -i "s/^Net\/IconThemeName.*/Net\/IconThemeName \"$icontheme\"/" ~/.config/xsettingsd/xsettingsd.conf
+gsettings set org.gnome.desktop.interface gtk-theme $theme
+gsettings set org.gnome.desktop.interface icon-theme $icontheme
 killall -HUP xsettingsd
