8519f039fa1555a01c0b15065d727dcaf393856e
samtrek <samtrek@yahoo.com>
2023-06-29 12:39:25 +0300

    made some modifications to darkman

--- darkman/dark-mode.d/gtk-theme.sh
+++ darkman/dark-mode.d/gtk-theme.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 theme=Nordic-darker
 icontheme=Papirus-Dark
+pkill -USR1 zsh
 brightnessctl set 20%
+killall -HUP xsettingsd
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^gtk-icon-theme-name=.*/gtk-icon-theme-name=$icontheme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord-dark\"/" ~/.config/rofi/config.rasi
@@ -11,5 +13,3 @@ sed -i "s/^Net\/ThemeName.*/Net\/ThemeName \"$theme\"/" ~/.config/xsettingsd/xse
 sed -i "s/^Net\/IconThemeName.*/Net\/IconThemeName \"$icontheme\"/" ~/.config/xsettingsd/xsettingsd.conf
 gsettings set org.gnome.desktop.interface gtk-theme $theme
 gsettings set org.gnome.desktop.interface icon-theme $icontheme
-killall -HUP xsettingsd
-pkill -USR1 zsh
--- darkman/light-mode.d/gtk-theme.sh
+++ darkman/light-mode.d/gtk-theme.sh
@@ -2,6 +2,8 @@
 theme=Nordic-Polar
 icontheme=Papirus-Dark
 brightnessctl set 50%
+killall -HUP xsettingsd
+pkill -USR1 zsh
 sed -i "s/^gtk-theme-name=.*/gtk-theme-name=$theme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^gtk-icon-theme-name=.*/gtk-icon-theme-name=$icontheme/" ~/.config/gtk-3.0/settings.ini
 sed -i "s/^@theme.*/@theme \"\/home\/samtrek\/\.local\/share\/rofi\/themes\/rounded-nord.rasi\"/" ~/.config/rofi/config.rasi
@@ -11,5 +13,3 @@ sed -i "s/^Net\/ThemeName.*/Net\/ThemeName \"$theme\"/" ~/.config/xsettingsd/xse
 sed -i "s/^Net\/IconThemeName.*/Net\/IconThemeName \"$icontheme\"/" ~/.config/xsettingsd/xsettingsd.conf
 gsettings set org.gnome.desktop.interface icon-theme $icontheme
 gsettings set org.gnome.desktop.interface gtk-theme $theme
-killall -HUP xsettingsd
-pkill -USR1 zsh
