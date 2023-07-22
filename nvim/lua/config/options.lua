62622a0213874cae8df064f9ea83f066fddaacdc
Taofeek Wahab <samtrek@yahoo.com>
2023-07-22 19:38:11 +0300

    changed the localleader in nvim to comma, since lazynvim has changed it to the default option

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,6 +1,7 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+vim.g.maplocalleader = ","
 vim.g["R_auto_start"] = 2
 vim.g["R_csv_app"] = "terminal:vd"
 vim.g["R_cmd"] = "R"
