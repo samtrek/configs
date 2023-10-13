f10a477d0bca8843f08bc37c455169e796111053
Taofeek Wahab <samtrek@yahoo.com>
2023-10-13 19:02:33 +0300

    made some changes to wezterm the font size and font type

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -13,4 +13,7 @@ return {
 	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.85,
+	text_background_opacity = 0.3,
+	font_size = 10,
+	font = wezterm.font("JetbrainsMono NF"),
 }
