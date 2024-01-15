2039f3a534056086b30fc9b935fb590101a7e1c6
samtrek <samtrek@yahoo.com>
2024-01-15 04:07:57 +0300

    add home monitor to config

--- darkman/dark-mode.d/wallpaper.sh
+++ darkman/dark-mode.d/wallpaper.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 
 pkill swaybg
-swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill &
+swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o HDMI-A-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill &
