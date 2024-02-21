b3359f2e06c2956df2d5b619dbc31413a1fb1d31
samtrek <samtrek@yahoo.com>
2024-02-21 07:43:57 +0300

    changed to the new nvim

---
+++ nvim/lua/plugins/r-nvim.lua
@@ -0,0 +1,40 @@
+return {
+  "jalvesaq/tmp-R-nvim",
+  config = function()
+    -- Create a table with the options to be passed to setup()
+    local opts = {
+      R_args = { "--quiet", "--no-save" },
+      R_app = "radian",
+      auto_start = 2,
+      Rout_more_colors = true,
+      hook = {
+        after_config = function()
+          -- This function will be called at the FileType event
+          -- of files supported by R.nvim. This is an
+          -- opportunity to create mappings local to buffers.
+          if vim.o.syntax ~= "rbrowser" then
+            vim.api.nvim_buf_set_keymap(0, "n", "<Enter>", "<Plug>RDSendLine", {})
+            vim.api.nvim_buf_set_keymap(0, "v", "<Enter>", "<Plug>RSendSelection", {})
+          end
+        end,
+      },
+      min_editor_width = 18,
+      rconsole_width = 50,
+      disable_cmds = {
+        "RClearConsole",
+        "RCustomStart",
+        "RSPlot",
+        "RSaveClose",
+      },
+    }
+    -- Check if the environment variable "R_AUTO_START" exists.
+    -- If using fish shell, you could put in your config.fish:
+    -- alias r "R_AUTO_START=true nvim"
+    if vim.env.R_AUTO_START == "true" then
+      opts.auto_start = 1
+      opts.objbr_auto_start = true
+    end
+    require("r").setup(opts)
+  end,
+  lazy = false,
+}
