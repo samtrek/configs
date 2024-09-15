f607a2ae9018a701c8ddff77ab347ae512d980d3
Taofeek Wahab <samtrek@yahoo.com>
2024-09-15 17:43:36 +0300

    removed darkman and marks from nvim

--- nvim/lua/plugins/darkman.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "4e554c4c/darkman.nvim",
-  build = "go build -o bin/darkman.nvim",
-  opts = {
-    change_background = true,
-    colorscheme = { dark = "catppuccin-frappe", light = "catppuccin-latte" },
-  },
-}
