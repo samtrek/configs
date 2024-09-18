1861ecd5ca88e99befc768e5216724101e8ca9a9
Taofeek Wahab <samtrek@yahoo.com>
2024-09-18 08:56:02 +0300

    changed to clipcat for clipboard

--- dwm-flexipatch/autostart.sh
+++ dwm-flexipatch/autostart.sh
@@ -1,64 +1,65 @@
 #!/bin/sh
 
 if pgrep -x slstatus >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	slstatus &
+  slstatus &
 fi
 
 if pgrep -x lxqt-powermanagement >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	lxqt-powermanagement &
+  lxqt-powermanagement &
 fi
 
 $HOME/.layout.sh &
 $HOME/.config/.fehbg &
 picom -b &
+clipcatd &
 nm-applet &
 thunar --deamon &
 caffeine start &
 
 if pgrep -x xob >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	$HOME/.config/sxhkd/pulse-volume-watcher.py | xob &
-	$HOME/.config/sxhkd/brightness-watcher.py | xob &
+  $HOME/.config/sxhkd/pulse-volume-watcher.py | xob &
+  $HOME/.config/sxhkd/brightness-watcher.py | xob &
 fi
 
 if pgrep -x blueman-applet >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	blueman-applet &
+  blueman-applet &
 fi
 
 if pgrep -x udiskie >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	udiskie -As &
+  udiskie -As &
 fi
 
 if pgrep -x nchat >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	kitty -1 --instance-group "kitty_sock" -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock --class nchat -o initial_window_width=65c -o initial_window_height=15c -e nchat &
+  kitty -1 --instance-group "kitty_sock" -o allow_remote_control=yes --listen-on unix:/tmp/kitty_sock --class nchat -o initial_window_width=65c -o initial_window_height=15c -e nchat &
 fi
 
 if pgrep -x syncthing-gtk >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	syncthing-gtk &
+  syncthing-gtk &
 fi
 
 if pgrep -x pomo.sh >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	pomo.sh start &
-	pomo.sh notif &
+  pomo.sh start &
+  pomo.sh notif &
 fi
 
 if pgrep -x xsettingsd >/dev/null; then
-	exit 1 &
+  exit 1 &
 else
-	xsettingsd &
+  xsettingsd &
 fi
