d42174906b30d00ef3706b0f54e0514c5e8b5247
Taofeek Wahab <samtrek@yahoo.com>
2024-10-31 15:42:20 +0300

    changed from swaybg to swww

--- darkman/dark-mode.d/wallpaper.sh
+++ darkman/dark-mode.d/wallpaper.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 
-pkill swaybg
-swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o HDMI-A-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill &
+# pkill swaybg
+# swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o HDMI-A-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill &
+swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/spy2.jpg
+swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon3.jpg
+swww img -o eDp-1 /home/samtrek/Documents/walpapers/demon3.jpg
--- darkman/light-mode.d/wallpaper.sh
+++ darkman/light-mode.d/wallpaper.sh
@@ -1,4 +1,8 @@
 #! /bin/sh
 
-pkill swaybg
-swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/demon.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg &
+# pkill swaybg
+# swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/demon.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg &
+
+swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
+swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
+swww img -o eDp-1 /home/samtrek/Documents/walpapers/luffy3.jpg
