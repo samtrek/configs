bed1e233bbef7c5abbc533cc17b4532b474d8e9c
Taofeek Wahab <samtrek@yahoo.com>
2024-10-31 16:07:44 +0300

    restart waybar with systemctl

--- darkman/dark-mode.d/waybar.sh
+++ darkman/dark-mode.d/waybar.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 ln -srf ~/.config/waybar/dark/style.css ~/.config/waybar/
-sleep 2 && killall -SIGUSR2 waybar
+systemctl --user restart waybar
+# sleep 2 && killall -SIGUSR2 waybar
--- darkman/light-mode.d/waybar.sh
+++ darkman/light-mode.d/waybar.sh
@@ -1,3 +1,4 @@
 #! /bin/sh
 ln -srf ~/.config/waybar/light/style.css ~/.config/waybar
-sleep 2 && killall -SIGUSR2 waybar
+systemctl --user restart waybar
+# sleep 2 && killall -SIGUSR2 waybar
