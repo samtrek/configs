f00fa6e932de641546a220adad166d0ff1175f9b
samtrek <samtrek@yahoo.com>
2024-11-01 07:47:33 +0300

    fixed swww with the laptop

--- darkman/dark-mode.d/wallpaper.sh
+++ darkman/dark-mode.d/wallpaper.sh
@@ -4,4 +4,4 @@
 # swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o HDMI-A-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill &
 swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/spy2.jpg
 swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon3.jpg
-swww img -o eDp-1 /home/samtrek/Documents/walpapers/demon3.jpg
+swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon3.jpg
--- darkman/light-mode.d/wallpaper.sh
+++ darkman/light-mode.d/wallpaper.sh
@@ -5,4 +5,4 @@
 
 swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
 swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
-swww img -o eDp-1 /home/samtrek/Documents/walpapers/luffy3.jpg
+swww img -o eDP-1 /home/samtrek/Documents/walpapers/luffy3.jpg
