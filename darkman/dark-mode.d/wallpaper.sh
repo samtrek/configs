1139c77bd937f1c457461befbd943abeabab74fb
Taofeek Wahab <samtrek@yahoo.com>
2024-11-11 17:40:07 +0300

    moved to hyprpaper from sww

--- darkman/dark-mode.d/wallpaper.sh
+++ darkman/dark-mode.d/wallpaper.sh
@@ -2,6 +2,9 @@
 
 # pkill swaybg
 # swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/spy2.jpg -m fill -o HDMI-A-1 -i /home/samtrek/Documents/walpapers/demon3.jpg -m fill &
-swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/spy2.jpg
-swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon3.jpg
-swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon3.jpg
+# swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/spy2.jpg
+# swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon3.jpg
+# swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon3.jpg
+hyprctl hyprpaper wallpaper "DVI-I-1, ~/Documents/walpapers/spy2.jpg"
+hyprctl hyprpaper wallpaper "DVI-I-2, ~/Documents/walpapers/demon3.jpg"
+hyprctl hyprpaper wallpaper "eDP-1, ~/Documents/walpapers/spy2.jpg"
