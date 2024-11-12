8a0a9e944b86d4148d232b2589671f8595302b64
samtrek <samtrek@yahoo.com>
2024-11-12 06:47:38 +0300

    moved to swww again hyprpaper was not effective

--- darkman/light-mode.d/wallpaper.sh
+++ darkman/light-mode.d/wallpaper.sh
@@ -3,9 +3,6 @@
 # pkill swaybg
 # swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/demon.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg &
 
-# swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
-# swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
-# swww img -o eDP-1 /home/samtrek/Documents/walpapers/luffy3.jpg
-hyprctl hyprpaper wallpaper "DVI-I-1, ~/Documents/walpapers/demon.jpg"
-hyprctl hyprpaper wallpaper "DVI-I-2, ~/Documents/walpapers/luffy3.jpg"
-hyprctl hyprpaper wallpaper "eDP-1, ~/Documents/walpapers/luffy3.jpg"
+swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
+swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
+swww img -o eDP-1 /home/samtrek/Documents/walpapers/luffy3.jpg
