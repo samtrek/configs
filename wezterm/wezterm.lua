fe8ee61ce2145c4659e646939afdeac1f540ad07
samtrek <samtrek@yahoo.com>
2024-11-16 09:41:27 +0300

    removed color in wezterm

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,16 +1,7 @@
 local wezterm = require("wezterm")
 
-function scheme_for_appearance(appearance)
-	if appearance:find("Dark") then
-		return "Catppuccin Frappe"
-	else
-		return "Catppuccin Latte"
-	end
-end
-
 return {
 	-- ...your existing config
-	color_scheme = scheme_for_appearance(wezterm.gui.get_appearance()),
 	hide_tab_bar_if_only_one_tab = true,
 	window_background_opacity = 0.85,
 	text_background_opacity = 0.3,
