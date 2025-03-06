414c9718fe071bdcb16f6bc6a92ebe1d7b5dea8c
Taofeek Wahab <samtrek@yahoo.com>
2025-03-06 17:04:33 +0300

    added augument-command

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,2 +1,24 @@
 require("starship"):setup()
 require("full-border"):setup()
+require("augment-command"):setup({
+	prompt = false,
+	default_item_group_for_prompt = "hovered",
+	smart_enter = true,
+	smart_paste = true,
+	smart_tab_create = false,
+	smart_tab_switch = false,
+	confirm_on_quit = true,
+	open_file_after_creation = false,
+	enter_directory_after_creation = false,
+	use_default_create_behaviour = false,
+	enter_archives = true,
+	extract_retries = 3,
+	recursively_extract_archives = true,
+	preserve_file_permissions = false,
+	must_have_hovered_item = true,
+	skip_single_subdirectory_on_enter = true,
+	skip_single_subdirectory_on_leave = true,
+	smooth_scrolling = false,
+	scroll_delay = 0.02,
+	wraparound_file_navigation = false,
+})
