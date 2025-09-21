965bab01ab6318d319024e731dc51a69f14699f3
Taofeek Wahab <samtrek@yahoo.com>
2025-09-21 08:03:26 +0300

    updated zotcite

--- nvim/lua/plugins/zotcite.lua
+++ nvim/lua/plugins/zotcite.lua
@@ -4,9 +4,4 @@ return {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim",
   },
-  config = function()
-    require("zotcite").setup({
-      -- your options here (see doc/zotcite.txt)
-    })
-  end,
 }
