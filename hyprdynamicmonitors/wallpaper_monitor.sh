73859a7a4c564f9441ba9a52b2aab66882a13df7
Taofeek Wahab <samtrek@yahoo.com>
2025-10-29 09:05:17 +0300

    fixed each wall paper hyprdynamic

--- hyprdynamicmonitors/wallpaper_monitor.sh
+++ hyprdynamicmonitors/wallpaper_monitor.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 
 if [ $(darkman get) = "light" ]; then
-  swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon.jpg
   swww img -o DP-2 /home/samtrek/Documents/walpapers/demon.jpg
 elif [ $(darkman get) = "dark" ]; then
-  swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon3.jpg
   swww img -o DP-2 /home/samtrek/Documents/walpapers/spy2.jpg
 fi
