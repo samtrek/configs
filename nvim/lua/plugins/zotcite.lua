e5fbce676478c1bad50acbbe3fd2a80161dd78f6
Taofeek Wahab <samtrek@yahoo.com>
2025-11-03 17:38:06 +0300

    add sql_path to zotcite to config

--- nvim/lua/plugins/zotcite.lua
+++ nvim/lua/plugins/zotcite.lua
@@ -1,7 +1,13 @@
 return {
   "jalvesaq/zotcite",
+  -- branch = "no_pynvim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim",
   },
+  config = function()
+    require("zotcite").setup({
+      SQL_path = "/home/samtrek/Zotero/zotero.sqlite",
+    })
+  end,
 }
