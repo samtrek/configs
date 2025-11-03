cbcceead9edf902af54d9a6122858016fa18f50a
Taofeek Wahab <samtrek@yahoo.com>
2025-11-03 04:42:21 +0300

    renamed blink_compact to blink and removed cmp_r

--- nvim/lua/plugins/blink.lua
+++ nvim/lua/plugins/blink.lua
@@ -1,10 +1,6 @@
 return {
   "saghen/blink.cmp",
-  dependencies = { "R-nvim/cmp-r", "saghen/blink.compat" },
   opts = {
-    sources = {
-      compat = { "cmp_r" },
-    },
     keymap = {
       preset = "enter",
       ["<Tab>"] = { "select_next", "fallback" },
