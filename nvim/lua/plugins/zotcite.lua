842c3a3e204a079a40df557f4d114ed492ffb21c
Taofeek Wahab <samtrek@yahoo.com>
2025-11-07 06:01:37 +0300

    made some adjstment to workspace 1

--- nvim/lua/plugins/zotcite.lua
+++ nvim/lua/plugins/zotcite.lua
@@ -1,12 +1,13 @@
 return {
   "jalvesaq/zotcite",
-  -- branch = "no_pynvim",
+  branch = "no_pynvim",
   dependencies = {
     "nvim-treesitter/nvim-treesitter",
     "nvim-telescope/telescope.nvim",
   },
   config = function()
     require("zotcite").setup({
+      dir = "/home/samtrek/.local/share/nvim/lazy/zotcite/",
       SQL_path = "/home/samtrek/Zotero/zotero.sqlite",
     })
   end,
