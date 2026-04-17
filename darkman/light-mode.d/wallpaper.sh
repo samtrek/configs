8f66126c904c7d8612bd1163c76c68737a2bf43d
Taofeek Wahab <samtrek@yahoo.com>
2026-04-17 20:24:04 +0100

    fixed apps after update

--- darkman/light-mode.d/wallpaper.sh
+++ darkman/light-mode.d/wallpaper.sh
@@ -3,7 +3,7 @@
 # pkill swaybg
 # swaybg -o DVI-I-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg -m fill -o DVI-I-2 -i /home/samtrek/Documents/walpapers/demon.jpg -m fill -o eDP-1 -i /home/samtrek/Documents/walpapers/luffy3.jpg &
 
-swww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
-swww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
-swww img -o eDP-1 /home/samtrek/Documents/walpapers/demon.jpg
-swww img -o DP-2 /home/samtrek/Documents/walpapers/demon.jpg
+awww img -o DVI-I-2 /home/samtrek/Documents/walpapers/luffy3.jpg
+awww img -o DVI-I-1 /home/samtrek/Documents/walpapers/demon.jpg
+awww img -o eDP-1 /home/samtrek/Documents/walpapers/demon.jpg
+awww img -o DP-2 /home/samtrek/Documents/walpapers/demon.jpg
